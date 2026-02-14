236a0551db6803c9fc163dbce8305889bd003d04
mlaska3000-prog <mlaska3000-prog@users.noreply.github.com>
2026-02-14 04:33:36 +0000

    Initial commit - app with bugs

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const users = [];
+app.post("/users", (req, res) => {
+  const user = { id: users.length + 1, name: req.body.name, email: req.body.email };
+  users.push(user);
+  res.json(user);
+});
+app.get("/users/:id", (req, res) => {
+  const user = users.find(u => u.id === req.params.id);
+  res.json(user);
+});
+app.get("/users", (req, res) => {
+  const page = req.query.page || 1;
+  const perPage = req.query.perPage || 10;
+  const start = (page - 1) * perPage;
+  const result = users.slice(start, start + perPage);
+  res.json({ users: result, total: users.length, page: page });
+});
+app.delete("/users/:id", (req, res) => {
+  const idx = users.findIndex(u => u.id === req.params.id);
+  if (idx !== -1) users.splice(idx, 1);
+  res.json({ deleted: true });
+});
+module.exports = app;
+if (require.main === module) app.listen(3000);
