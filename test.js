aa2ae62c6f89df3d51bbaac99b1f5746503bcec9
mlaska3000-prog <mlaska3000-prog@users.noreply.github.com>
2026-02-19 14:02:26 +0000

    chore: clean up test.js - remove debug code

--- test.js
+++ test.js
@@ -24,5 +24,3 @@ describe("GET /health", function() {
 // Bug 1: GET /users/:id - id comparison is string vs number (will always return undefined)
 // Bug 2: DELETE /users/:id - same string vs number comparison
 // Bug 3: Pagination - page/perPage are strings, math will be wrong
-console.log("Tests loaded");
-process.exit(0);
